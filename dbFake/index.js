d00f985d93878344d2e00c393b89c91e2ddab88d
AlanSoares21 <alansoares26@outlook.com>
2020-07-19 16:48:21 -0300

    Criando metodos para salvar os registros dos usuarios

--- dbFake/index.js
+++ dbFake/index.js
@@ -1,23 +1,31 @@
 import fs from 'fs';
+import {resolve} from 'path';
 
-const dataPath = './data';
+const dataPath = resolve(__dirname,'./data.json');
 const options = {encoding:'utf-8'};
 
 function getData(){
-    const file = fs.readFileSync(dataPath,options);
+    const file = JSON.parse(fs.readFileSync(dataPath,options));
     return file;
 }
 function saveData(data){
     try{
-        fs.writeFileSync(dataPath,options);
+        fs.writeFileSync(dataPath,JSON.stringify(data),options);
     }catch(err){
-        console.error('Erro ao salvar dados');
+        console.error('Erro ao salvar dados: '+err);
     }
 }
 
 export default {
     async createUser(dataUser){
-        
+        const users = getData();
+        const user = {
+            ...dataUser, 
+            id: users.length===0? 0 : ( users[users.length-1].id + 1 ) 
+        };
+        users.push(user);
+        saveData(users);
+        return user;
     },
     async getUser(id){
 
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,16 +1,18 @@
 import passwordGenerator from 'password-generator';
 import Queue from '../lib/Queue';
 
+import db from '../../../dbFake';
+
 export default {
     async store(req,res){
         // console.log("criando usuario");
-        const {name,email, id} = req.body;
-        const user = {
-            id,
+        const {name,email} = req.body;
+        const user = await db.createUser( {
             name,
             email,
             password : passwordGenerator(15,false)
-        }
+        });
+        
         // console.log("enviando email");
         await Queue.add('RegistrationMail',{user});
         // console.log("job na fila");
